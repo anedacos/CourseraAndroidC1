ba34bf37c9f0c04510eeb34d8f470752595fa2b6
anedacos <anedacos@espol.edu.ec>
2021-04-12 20:32:31 -0500

    Proyecto Semana 3

--- Programas/Netflix/src/com/anthonyacosta/netflix/Netflix.java
+++ Programas/Netflix/src/com/anthonyacosta/netflix/Netflix.java
@@ -1,6 +1,6 @@
 package com.anthonyacosta.netflix;
 
-//import java.util.ArrayList;
+import java.util.ArrayList;
 
 public class Netflix {
 
@@ -11,54 +11,55 @@ public class Netflix {
 		int p=0;
 		int s=0;
 		
-		//ArrayList<Pelicula> peliculas = new ArrayList<Pelicula>();
-		Pelicula peliculas[]= new Pelicula[5];
-		Serie series[]=new Serie[5];
+		ArrayList<Pelicula> peliculas = new ArrayList<Pelicula>();
+		ArrayList<Serie> series = new ArrayList<Serie>();
 		
-		peliculas[0]=new Pelicula("El matata","Ciencia ficcion","Robert Denilo",2018,210);
-		peliculas[1]=new Pelicula("Rescate en Nueva York","accion","Robert Denilo",2020,142);
-		peliculas[2]=new Pelicula("Golpe en la pequeña China","drama","John Carpenter",1995,125);
-		peliculas[3]=new Pelicula("Arma Letal'","Suspenso","Richard Donner",2019,205);
-		peliculas[4]=new Pelicula("Jungla de Cristal","comedia","John McTiernan",1990,192);
-		
-		series[0]= new Serie("A dos metros bajo tierra","drama","Alan Ball",4,500);
-		series[1]= new Serie("El ala oeste de la Casa Blanca","idealista","Aaron Sorkin ",5,550);
-		series[2]= new Serie("Alfred Hitchcock presenta","suspenso","Alfred Hitchcock",12,220);
-		series[3]= new Serie("Battlestar Galactica","ciencia ficcion","Ronald D. Moore",11,453);
-		series[4]= new Serie("Bojack Horseman","animada","Raphael Bob-Waksberg",10,700);
-		
-		peliculas[0].marcarVisto();
-		peliculas[3].marcarVisto();
-		series[1].marcarVisto();
-		series[4].marcarVisto();
-		
-		temp=peliculas[0].anio;
+		peliculas.add(new Pelicula("El matata","Ciencia ficcion","Robert Denilo",2018,210));
+		peliculas.add(new Pelicula("Rescate en Nueva York","accion","Robert Denilo",2020,142));
+		peliculas.add(new Pelicula("Golpe en la pequeña China","drama","John Carpenter",1995,125));
+		peliculas.add(new Pelicula("Arma Letal'","Suspenso","Richard Donner",2019,205));
+		peliculas.add(new Pelicula("Jungla de Cristal","comedia","John McTiernan",1990,192));
 
-		for (int i = 0; i<peliculas.length;i++) {
-			if(peliculas[i].visto) {
-				System.out.println(peliculas[i].toString());
+		series.add(new Serie("A dos metros bajo tierra","drama","Alan Ball",4,500));
+		series.add(new Serie("El ala oeste de la Casa Blanca","idealista","Aaron Sorkin ",5,550));
+		series.add(new Serie("Alfred Hitchcock presenta","suspenso","Alfred Hitchcock",12,220));
+		series.add(new Serie("Battlestar Galactica","ciencia ficcion","Ronald D. Moore",11,453));
+		series.add(new Serie("Bojack Horseman","animada","Raphael Bob-Waksberg",10,700));
+		try {
+			peliculas.get(0).marcarVisto();
+			peliculas.get(3).marcarVisto();
+			series.get(1).marcarVisto();
+			series.get(4).marcarVisto();
+			
+			temp=peliculas.get(0).anio;
+			
+			for (int i = 0; i<peliculas.size();i++) {
+				if(peliculas.get(i).visto) {
+					System.out.println(peliculas.get(i).toString());
+				}
+				if(peliculas.get(i).anio>temp) {
+					temp=peliculas.get(i).anio;
+					p=i;
+				}
 			}
-			if(peliculas[i].anio>temp) {
-				temp=peliculas[i].anio;
-				p=i;
+			
+			temp=series.get(0).noTemporadas;
+			
+			for(int i=0; i<series.size();i++) {
+				if(series.get(i).visto) {
+					System.out.println(series.get(i).toString());
+				}
+				if(series.get(i).noTemporadas>temp) {
+					temp=series.get(i).noTemporadas;
+					s=i;
+				}
 			}
+			
+			System.out.println("\nPelicula mas reciente:\n"+peliculas.get(p).toString());	
+			System.out.println("\nSerie con mas temporadas:\n"+series.get(s).toString());
+		} catch (Exception e) {
+			System.out.println("Ocurrio un error");
 		}
-		
-		temp=series[0].noTemporadas;
-		
-		for(int i=0; i<series.length;i++) {
-			if(series[i].visto) {
-				System.out.println(series[i].toString());
-			}
-			if(series[i].noTemporadas>temp) {
-				temp=series[i].noTemporadas;
-				s=i;
-			}
-		}
-		
-		
-		System.out.println("\nPelicula mas reciente:\n"+peliculas[p].toString());	
-		System.out.println("\nSerie con mas temporadas:\n"+series[s].toString());
 		
 	}
 
